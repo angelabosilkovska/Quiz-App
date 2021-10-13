a97595dbc550199b4ccb478703d7ebea94627f8a
Angela Bosilkovska <angela.bosilkovska@uklo.edu.mk>
2021-10-13 13:53:55 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <b-nav tabs>
+    <b-nav-item disable><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disablel>Counter: 1/4</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
